d2abd6789446f161711cb09e73f9a83e620b6b49
Yahor Maksimchyk <yahor.maksimchyk@gmail.com>
2025-02-17 14:55:21 +0300

    task variables

---
+++ js-core-advanced/variables/index.js
@@ -0,0 +1,17 @@
+
+console.log(a);
+var a = 10;
+
+// console.log(b);
+//             ^
+
+// ReferenceError: Cannot access 'b' before initialization
+let b = 20;
+
+// console.log(c);
+//             ^
+
+// ReferenceError: Cannot access 'c' before initialization
+const c = 30;
+
+// variables hoisting
